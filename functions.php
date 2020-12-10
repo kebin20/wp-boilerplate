88248f778c52f7e6c158ffcec2974c2ee315b2f0
Sean V <seanv@hotmail.co.nz>
2020-12-10 11:37:06 +0900

    Combined 'page' and 'single' into 'index'. Removed custom is_mobile() function.

--- functions.php
+++ functions.php
@@ -199,33 +199,6 @@ function theme_slug_filter_wp_title($title_parts)
 }
 add_filter('document_title_parts', 'theme_slug_filter_wp_title');
 
-
-/* Custom is_mobile() function
-デバイス条件
------------------------------------------------ */
-function is_mobile()
-{
-    $useragents = array(
-        'iPhone', // iPhone
-        'iPod', // iPod touch
-        'Android.*Mobile', // 1.5+ Android *** Only mobile
-        'Windows.*Phone', // *** Windows Phone
-        'dream', // Pre 1.5 Android
-        'CUPCAKE', // 1.5+ Android
-        'blackberry9500', // Storm
-        'blackberry9530', // Storm
-        'blackberry9520', // Storm v2
-        'blackberry9550', // Storm v2
-        'blackberry9800', // Torch
-        'webOS', // Palm Pre Experimental
-        'incognito', // Other iPhone browser
-        'webmate' // Other iPhone browser
-    );
-    $pattern = '/' . implode('|', $useragents) . '/i';
-    return preg_match($pattern, $_SERVER['HTTP_USER_AGENT']);
-}
-
-
 /* Retrieve page URL using slug
 ページスラグで URLを得るファンクション
 ----------------------------------------------- */
--- index.php
+++ index.php
@@ -11,10 +11,14 @@
 get_header();
 ?>
 
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-        <?php endwhile; ?>
-    <?php endif; ?>
+    <?php
+    if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+            the_content();
+        endwhile;
+    endif;
+    ?>
 
 <?php
 get_footer();
