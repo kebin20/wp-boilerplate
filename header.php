8bb5691178ee4c7da64089d306b3332622b456e1
Sean V <seanv@hotmail.co.nz>
2020-12-21 11:54:54 +0900

    Minimilized SCSS _tools and _objects files

--- header.php
+++ header.php
@@ -16,8 +16,6 @@ $bodyClass = array();
 <html <?php language_attributes(); ?>>
 
 <head>
-
-
     <meta charset="<?php bloginfo("charset"); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
