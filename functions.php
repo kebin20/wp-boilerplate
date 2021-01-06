4697b38a21fcf5837759013e460ed01f76196589
Sean V <seanv@hotmail.co.nz>
2021-01-06 18:38:47 +0900

    Fixed comments

--- functions.php
+++ functions.php
@@ -93,9 +93,9 @@ add_action( 'wp_enqueue_scripts', 'move_jquery_to_footer' );
 
 /* Queueing for Header scripts and styles.
 ----------------------------------------------- */
-remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('wp_print_styles', 'print_emoji_styles'); // Emoji に関するファイルを読み込まないように
 remove_action('wp_head', 'print_emoji_detection_script', 7); // Emoji に関するファイルを読み込まないように
-function gutenbase_enqueue_assets() // Emoji に関するファイルを読み込まないように
+function gutenbase_enqueue_assets()
 {
     if (!is_admin()) {
         //== DEFAULT JS
