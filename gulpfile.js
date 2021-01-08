d7b3db757d77fdd43cdada323b4e3ef45ff03fbe
Sean V <seanv@hotmail.co.nz>
2021-01-08 11:44:05 +0900

    Formatted gulpfile and integrated BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -17,62 +17,60 @@ const beeper = require('beeper');
 // Compile CSS from Sass.
 function buildStyles() {
     return src('scss/style.scss')
-    .pipe(plumbError()) // Global error handler through all pipes.
-    //.pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'compressed', indentWidth: 4, }))
-    .pipe(autoprefixer(['last 2 versions', '> 1%', 'ie 11']))
-    //.pipe(sourcemaps.write())
-    .pipe(dest('.'))
-    .pipe(browsersync.reload({ stream: true }));
+        .pipe(plumbError()) // Global error handler through all pipes.
+        //.pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed', indentWidth: 4, }))
+        .pipe(autoprefixer(['last 2 versions', '> 1%', 'ie 11']))
+        //.pipe(sourcemaps.write())
+        .pipe(dest('.'))
+        .pipe(browsersync.reload({ stream: true }));
 }
 
 // Watch changes on all *.scss files, lint them and
 // trigger buildStyles() at the end.
 function watchFiles() {
-  watch(
-    ['scss/*.scss', 'scss/**/*.scss'],
-    { events: 'all', ignoreInitial: false },
-    series(sassLint, buildStyles)
-  );
+    watch(
+        ['scss/*.scss', 'scss/**/*.scss'],
+        { events: 'all', ignoreInitial: false },
+        series(sassLint, buildStyles, browserSync)
+    );
 }
 
 // Init BrowserSync.
 function browserSync(done) {
-  browsersync.init({
-    proxy: 'localhost', // Change this value to match your local URL.
-    socket: {
-      domain: 'localhost:8888'
-    }
-  });
-  done();
+    browsersync.init({
+        proxy: 'localhost:3000/gutenbase-wp/', // Change this value to match your local URL.
+        browser: "google chrome",
+    });
+    done();
 }
 
 // Init Sass linter.
 function sassLint() {
-  return src(['scss/*.scss', 'scss/**/*.scss'])
-    .pipe(cache('sasslint'))
-    .pipe(sasslint({
-      configFile: '.sass-lint.yml'
-    }))
-    .pipe(sasslint.format())
+    return src(['scss/*.scss', 'scss/**/*.scss'])
+        .pipe(cache('sasslint'))
+        .pipe(sasslint({
+            configFile: '.sass-lint.yml'
+        }))
+        .pipe(sasslint.format())
     //.pipe(sasslint.failOnError()); //no failure on SASS Linting error
 }
 
 // Error handler.
 function plumbError() {
-  return plumber({
-    errorHandler: function(err) {
-      notify.onError({
-        templateOptions: {
-          date: new Date()
-        },
-        title: "Gulp error in " + err.plugin,
-        message:  err.formatted
-      })(err);
-      beeper();
-      this.emit('end');
-    }
-  })
+    return plumber({
+        errorHandler: function (err) {
+            notify.onError({
+                templateOptions: {
+                    date: new Date()
+                },
+                title: "Gulp error in " + err.plugin,
+                message: err.formatted
+            })(err);
+            beeper();
+            this.emit('end');
+        }
+    })
 }
 
 // Export commands.
