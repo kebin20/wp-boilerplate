72179d69449fade76f41cb766ddfb64b9bd1e1f7
Sean Verity <seanv@hotmail.co.nz>
2021-02-02 17:10:46 +0900

    Added try-catch for all functions

--- js/site-init.js
+++ js/site-init.js
@@ -5,36 +5,58 @@
  * Insert code to be loaded last here. jQuery has been loaded by the time this script is called.
  */
 
+
+/* Launch Site
+----------------------------------------------- */
+try {
+    slickInitiate();
+} catch (err) {
+    console.log("slickInitiate Error: " + err + ".");
+}
+
+try {
+    aosInitiate();
+} catch (err) {
+    console.log("aosInitiate Error: " + err + ".");
+}
+
+
+
+
 /* Slick Slider
 ----------------------------------------------- */
-if (jQuery('.slick-slider')) {
-    jQuery('.slick-slider').each(function() {
-        var slider = jQuery(this);
-        if (slider.children().length > 1) {
-            slider.slick({
-                arrows: true,
-                dots: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                autoplay: true,
-                autoplaySpeed: 4000,
-                speed: 1000,
-                fade: true,
-                infinite: true,
-                loop: true,
-            });
-        }
-    });
+function slickInitiate() {
+    if (jQuery('.slick-slider')) {
+        jQuery('.slick-slider').each(function () {
+            var slider = jQuery(this);
+            if (slider.children().length > 1) {
+                slider.slick({
+                    arrows: true,
+                    dots: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 4000,
+                    speed: 1000,
+                    fade: true,
+                    infinite: true,
+                    loop: true,
+                });
+            }
+        });
+    }
 }
 
 /* AOS
 ----------------------------------------------- */
-//Must be initiated after slick if there are animations in the slider
-if(typeof AOS != "undefined"){
-    AOS.init({
-        delay: 0, // values from 0 to 3000, with step 50ms
-        duration: 1000, // values from 0 to 3000, with step 50ms
-        easing: 'ease', // default easing for AOS animations
-        once: true, // whether animation should happen only once - while scrolling down
-    });
+function aosInitiate() {
+    //Must be initiated after slick if there are animations in the slider
+    if (typeof AOS != "undefined") {
+        AOS.init({
+            delay: 0, // values from 0 to 3000, with step 50ms
+            duration: 1000, // values from 0 to 3000, with step 50ms
+            easing: 'ease', // default easing for AOS animations
+            once: true, // whether animation should happen only once - while scrolling down
+        });
+    }
 }
