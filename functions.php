f67c74220d217a6e1a698299ff952a1186824072
veritasnz <7684225+veritasnz@users.noreply.github.com>
2021-08-27 21:19:07 +0900

    Removed content-width  and comments features

--- functions.php
+++ functions.php
@@ -48,26 +48,18 @@ if (!function_exists('boilerplate_setup')) :
             'gallery',
             'caption',
         ));
+
+        /*
+		 * Removes comment feature from post and pages
+		 */
+        add_action('init', function () {
+            remove_post_type_support('post', 'comments');
+            remove_post_type_support('page', 'comments');
+        }, 100);
     }
 endif;
 
 add_action('after_setup_theme', 'boilerplate_setup');
-
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function boilerplate_content_width()
-{
-    // This variable is intended to be overruled from themes.
-    // Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-    // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-    $GLOBALS['content_width'] = apply_filters('boilerplate_content_width', 640);
-}
-add_action('after_setup_theme', 'boilerplate_content_width', 0);
 
 
 
